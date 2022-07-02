6c251f81fab239293578b7aa2fc8501d0b116607
Alexander <alyokhin22@gmail.com>
2022-07-02 09:25:55 +0300

    Hidden footer & add hire link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,7 @@ const config = {
         },
         items: [
           {to: '/blog', label: 'Новости', position: 'left'},
+          {to: '/hire', label: 'Присоединиться', position: 'left'},
           {
             href: 'https://github.com/hoolie-projects',
             label: 'GitHub',
@@ -68,56 +69,56 @@ const config = {
           },
         ],
       },
-      footer: {
-        // style: 'dark',
-        // links: [
-        //   {
-        //     title: 'Docs',
-        //     items: [
-        //       {
-        //         label: 'Tutorial',
-        //         to: '/docs/intro',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'Community',
-        //     items: [
-        //       {
-        //         label: 'Stack Overflow',
-        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //       },
-        //       {
-        //         label: 'Discord',
-        //         href: 'https://discordapp.com/invite/docusaurus',
-        //       },
-        //       {
-        //         label: 'Twitter',
-        //         href: 'https://twitter.com/docusaurus',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'More',
-        //     items: [
-        //       {
-        //         label: 'Blog',
-        //         to: '/blog',
-        //       },
-        //       {
-        //         label: 'GitHub',
-        //         href: 'https://github.com/facebook/docusaurus',
-        //       },
-        //     ],
-        //   },
-        // ],
-        copyright: `Copyright © ${new Date().getFullYear()} Hoolie Projects`,
-      },
+      // footer: {
+      //   // style: 'dark',
+      //   // links: [
+      //   //   {
+      //   //     title: 'Docs',
+      //   //     items: [
+      //   //       {
+      //   //         label: 'Tutorial',
+      //   //         to: '/docs/intro',
+      //   //       },
+      //   //     ],
+      //   //   },
+      //   //   {
+      //   //     title: 'Community',
+      //   //     items: [
+      //   //       {
+      //   //         label: 'Stack Overflow',
+      //   //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //   //       },
+      //   //       {
+      //   //         label: 'Discord',
+      //   //         href: 'https://discordapp.com/invite/docusaurus',
+      //   //       },
+      //   //       {
+      //   //         label: 'Twitter',
+      //   //         href: 'https://twitter.com/docusaurus',
+      //   //       },
+      //   //     ],
+      //   //   },
+      //   //   {
+      //   //     title: 'More',
+      //   //     items: [
+      //   //       {
+      //   //         label: 'Blog',
+      //   //         to: '/blog',
+      //   //       },
+      //   //       {
+      //   //         label: 'GitHub',
+      //   //         href: 'https://github.com/facebook/docusaurus',
+      //   //       },
+      //   //     ],
+      //   //   },
+      //   // ],
+      //   // copyright: `Copyright © ${new Date().getFullYear()} Hoolie Projects`,
+      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
-};
+}
 
 module.exports = config;
