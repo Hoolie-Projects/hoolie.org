1940f3ed45c40f1baa7791b6180e42b1673cd223
alyokhin22 <alyokhin22@users.noreply.github.com>
2022-07-07 23:37:06 +0000

    deploy: 5b7ebbbcd28954b9088f802cc3284abae1595f0f

---
+++ assets/js/814f3328.05fe8340.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhoolie_org=self.webpackChunkhoolie_org||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"CRA Scaffold","permalink":"/blog/cra-scaffold"},{"title":"\u041d\u043e\u0432\u044b\u0439 \u0441\u0430\u0439\u0442","permalink":"/blog/new-site"}]}')}}]);
